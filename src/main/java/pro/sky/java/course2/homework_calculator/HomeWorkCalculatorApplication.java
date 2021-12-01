4e925fb1a331049b5ec4d0ed5aaaee41cb427ad3
RickLyudmila <Lrick@yandex.ru>
2021-12-01 22:42:16 +0300

    first commit for calculator

---
+++ src/main/java/pro/sky/java/course2/homework_calculator/HomeWorkCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homework_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/homework_calculator/HomeWorkCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homework_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
